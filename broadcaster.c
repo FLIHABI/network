827eb1ce37579d1ab5684e6ab4a2b0e5e9113c7a
Yayg <rafael.gozlan@wanadoo.fr>
2015-03-13 07:02:44 +0000

    Add 255.255.255.255 as default broadcast ip

--- broadcaster.c
+++ broadcaster.c
@@ -1,5 +1,5 @@
 /*
-** broadcaster.c -- a datagram "client" like talker.c, except
+** broadcaster.c -- a datagram "client"
 **                  this one can broadcast
 */
 
@@ -14,54 +14,61 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 
-#define SERVERPORT 4950	// the port users will be connecting to
+#define SERVERPORT 4950    // the port users will be connecting to
+
+int broadcast(char *ip, char *msg)
+{
+    int sockfd;
+    struct sockaddr_in their_addr; // connector's address information
+    struct hostent *he;
+    int numbytes;
+    int broadcast = 1;
+    //char broadcast = '1'; // if that doesn't work, try this
+
+    if ((he=gethostbyname(ip)) == NULL) {  // get the host info
+        perror("gethostbyname");
+        exit(1);
+    }
+
+    if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
+        perror("socket");
+        exit(1);
+    }
+
+    // this call is what allows broadcast packets to be sent:
+    if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcast,
+        sizeof broadcast) == -1) {
+        perror("setsockopt (SO_BROADCAST)");
+        exit(1);
+    }
+
+    their_addr.sin_family = AF_INET;     // host byte order
+    their_addr.sin_port = htons(SERVERPORT); // short, network byte order
+    their_addr.sin_addr = *((struct in_addr *)he->h_addr);
+    memset(their_addr.sin_zero, '\0', sizeof their_addr.sin_zero);
+
+    if ((numbytes=sendto(sockfd, msg, strlen(msg), 0,
+             (struct sockaddr *)&their_addr, sizeof their_addr)) == -1) {
+        perror("sendto");
+        exit(1);
+    }
+
+    printf("sent %d bytes to %s\n", numbytes,
+        inet_ntoa(their_addr.sin_addr));
+
+    close(sockfd);
+
+    return 0;
+
+}
 
 int main(int argc, char *argv[])
 {
-	int sockfd;
-	struct sockaddr_in their_addr; // connector's address information
-	struct hostent *he;
-	int numbytes;
-	int broadcast = 1;
-	//char broadcast = '1'; // if that doesn't work, try this
+    if (argc != 2) {
+        fprintf(stderr,"usage: broadcast message\n");
+        exit(1);
+    }
 
-	if (argc != 3) {
-		fprintf(stderr,"usage: broadcaster hostname message\n");
-		exit(1);
-	}
-
-	if ((he=gethostbyname(argv[1])) == NULL) {  // get the host info
-		perror("gethostbyname");
-		exit(1);
-	}
-
-	if ((sockfd = socket(AF_INET, SOCK_DGRAM, 0)) == -1) {
-		perror("socket");
-		exit(1);
-	}
-
-	// this call is what allows broadcast packets to be sent:
-	if (setsockopt(sockfd, SOL_SOCKET, SO_BROADCAST, &broadcast,
-		sizeof broadcast) == -1) {
-		perror("setsockopt (SO_BROADCAST)");
-		exit(1);
-	}
-
-	their_addr.sin_family = AF_INET;	 // host byte order
-	their_addr.sin_port = htons(SERVERPORT); // short, network byte order
-	their_addr.sin_addr = *((struct in_addr *)he->h_addr);
-	memset(their_addr.sin_zero, '\0', sizeof their_addr.sin_zero);
-
-	if ((numbytes=sendto(sockfd, argv[2], strlen(argv[2]), 0,
-			 (struct sockaddr *)&their_addr, sizeof their_addr)) == -1) {
-		perror("sendto");
-		exit(1);
-	}
-
-	printf("sent %d bytes to %s\n", numbytes,
-		inet_ntoa(their_addr.sin_addr));
-
-	close(sockfd);
-
-	return 0;
+    broadcast("255.255.255.255", argv[1]);
+    return 0;
 }
