223408841d9bd1057c5ea9eb56405d32d3269d8f
Rafael Gozlan <rafael.gozlan@epita.fr>
2015-04-25 18:49:55 +0000

    Add tests for master slave

---
+++ tests/server_main.cc
@@ -0,0 +1,21 @@
+/*
+ * ============================================================================
+ *       Filename:  server_main.cc
+ *    Description:  Server main for testing
+ *        Version:  1.0
+ *        Created:  04/25/2015 06:37:53 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  Rafael Gozlan, rafael.gozlan@epita.fr
+ *   Organization:  Flihabi
+ * ============================================================================
+ */
+#include <stdlib.h>
+#include <unistd.h>
+#include "../src/server.hh"
+
+int main()
+{
+    Server s(4545);
+    sleep(5);
+}
