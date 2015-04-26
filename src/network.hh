04f338acbb433c6b42dc7c6d22fd596477680797
Yayg <rafael.gozlan@wanadoo.fr>
2015-04-26 01:37:12 +0000

    Add network spec file

---
+++ src/network.hh
@@ -0,0 +1,23 @@
+/*
+ * ============================================================================
+ *       Filename:  network.hh
+ *    Description:  Network specifications
+ *        Version:  1.0
+ *        Created:  04/25/2015 07:28:08 PM
+ *       Revision:  none
+ *       Compiler:  gcc
+ *         Author:  Rafael Gozlan, rafael.gozlan@epita.fr
+ *   Organization:  Flihabi
+ * ============================================================================
+ */
+
+
+#ifndef  NETWORK_INC
+#define  NETWORK_INC
+
+#define BROADCAST_PORT 4545
+#define BROADCAST_MSG "Flihabi: Hello!"
+#define CONNECTION_PORT 4242
+#define CONNECTION_MSG "Flihabi: Connection"
+
+#endif   /* ----- #ifndef NETWORK_INC  ----- */
