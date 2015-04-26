fe2b3bf945395f02bffe7161b837a376ea6496c0
Yayg <rafael.gozlan@wanadoo.fr>
2015-04-26 03:43:18 +0000

    Add slave ctor

--- src/slave.hxx
+++ src/slave.hxx
@@ -11,10 +11,104 @@
  * ============================================================================
  */
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
 #include "slave.hh"
+#include "listener.hh"
+
+// PUBLIC METHODS
 
 Slave::Slave()
 {
-    // FIXME: Get server address
-    // FIXME: Connect to server and hold connection
+    std::string ip = getServerAddress();
+    connectToServer(ip, CONNECTION_PORT);
+}
+
+std::string Slave::getBytecode()
+{
+    return "";
+}
+
+
+void Slave::send_bytecode(std::string bytecode)
+{
+    bytecode = "";
+}
+
+// PRIVATE METHODS
+
+std::string Slave::getServerAddress()
+{
+    Socket *s = listen(BROADCAST_PORT);
+    close(s->sockfd);
+    return s->msg;
+}
+
+void Slave::connectToServer(std::string ip, int port)
+{
+    int sockfd, numbytes;
+    char buf[100];
+    struct addrinfo hints, *servinfo, *p;
+    int rv;
+    char s[INET6_ADDRSTRLEN];
+
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+    if ((rv = getaddrinfo(ip.c_str(), std::to_string(port).c_str(), &hints, &servinfo)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
+
+    // loop through all the results and connect to the first we can
+    for(p = servinfo; p != NULL; p = p->ai_next) {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                p->ai_protocol)) == -1) {
+            perror("client: socket");
+            continue;
+        }
+
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+            close(sockfd);
+            perror("client: connect");
+            continue;
+        }
+
+        break;
+    }
+
+    if (p == NULL) {
+        fprintf(stderr, "client: failed to connect\n");
+        return 2;
+    }
+
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+            s, sizeof s);
+    printf("client: connecting to %s\n", s);
+
+    freeaddrinfo(servinfo); // all done with this structure
+
+    if ((numbytes = recv(sockfd, buf, MAXDATASIZE-1, 0)) == -1) {
+        perror("recv");
+        exit(1);
+    }
+
+    buf[numbytes] = '\0';
+
+    printf("client: received '%s'\n",buf);
+
+    close(sockfd);
+
+    return 0;
 }
