a07843a8ba5b223dd29b3b043c38b868177aaae5
Neodyblue <sieramsn@gmail.com>
2015-06-17 20:26:44 +0200

    Added network mode enum

---
+++ include/network_mode.hh
@@ -0,0 +1,13 @@
+#ifndef NETWORK_MODE_HH
+# define NETWORK_MODE_HH
+
+namespace network
+{
+  enum NetworkMode
+  {
+    SERVER,
+    CLIENT
+  };
+}
+
+#endif /* end of include guard: NETWORK_MODE_HH */
